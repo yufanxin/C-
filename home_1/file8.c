d1b1b8099d0cfe1f96b1b4b15aeedc2fc288ef96
Fanxin Yu <isyufanxin@gmail.com>
2017-11-07 09:57:06 +0800

    Create file8.c

---
+++ home_1/file8.c
@@ -0,0 +1,23 @@
+//输入两个数，用指针的方法先大后小输出
+
+#include <stdio.h>
+
+int main(void)
+{
+	int *p=NULL;
+	int *q=NULL;
+	int *tab=NULL;
+	int number1=0;
+	int number2=0;
+	printf("请输入两个数");
+	scanf("%d %d",&number1,&number2);
+	p=&number1;
+	q=&number2; 
+	if(number1<number2) //交换两数字指针地址
+	{
+		tab=p;
+		p=q;
+		q=tab;
+	}
+	printf("Max=%d,Min=%d\n",*p,*q);
+ } 
