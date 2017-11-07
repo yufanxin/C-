ed2313fb2a194478f86e1ba408433e620130a6a3
Fanxin Yu <isyufanxin@gmail.com>
2017-11-07 19:08:35 +0800

    Create file4.c

---
+++ home_2/file4.c
@@ -0,0 +1,18 @@
+ 
+#include <stdio.h>
+int main()
+{
+	int i=0;
+	int *p=NULL;
+	int a[8]={12,2,13,24,1,24,3,23};
+	p=a;
+	for(i=0;i<8;i++)
+	{
+		printf("a[i]=%d\n",p[i]);
+	}
+	printf("\n");
+	for(i=0;i<8;i++)
+	{
+		printf("*(p+%d):%d\n",i,*(p+i));
+	}
+}
